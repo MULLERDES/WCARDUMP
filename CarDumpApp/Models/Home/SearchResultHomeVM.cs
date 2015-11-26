c80bcd4735d13d9c4d9d776912537b09190c3856
Andrew Muller <amelnikd@gmail.com>
2015-11-26 10:18:47 +0200

    fasfas

---
+++ CarDumpApp/Models/Home/SearchResultHomeVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarDumpApp.Models
+{
+    public class SearchResultHomeVM
+    {
+        private int perpagecount = 10;
+        public List<CarDump> Cardumps { get; set; }
+        public int CurrentPage { get; set; } = 0;
+        public int MaximunPages { get; set; }
+
+        public string SearchParameters { get; set; }
+    }
+}
